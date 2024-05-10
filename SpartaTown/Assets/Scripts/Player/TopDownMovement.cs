6ce3f8179a9c31f7f069e2e1ec64aa4d22ae4727
ghdwl <ghdwls4654@naver.com>
2024-05-10 15:31:34 +0900

    캐릭터 생성, 캐릭터 이동
    캐릭터 생성 및 애니메이션 적용, 이동 구현

---
+++ SpartaTown/Assets/Scripts/Player/TopDownMovement.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using UnityEngine;
+
+using Debug = UnityEngine.Debug;
+
+// TopDownMovement는 캐릭터와 몬스터의 이동에 사용될 예정입니다.
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController movementController;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+    private Vector2 mouseDirection = Vector2.zero;
+    private Animator animator;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private void Awake()
+    {
+        // 같은 게임오브젝트의 TopDownController, Rigidbody를 가져올 것 
+        movementController = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        // OnMoveEvent에 Move를 호출하라고 등록함
+        movementController.OnMoveEvent += Move;
+        movementController.OnLookEvent += Look;
+    }
+
+    private void FixedUpdate()
+    {
+        // 물리 업데이트에서 움직임 적용
+        ApplyMovement(movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        // 이동방향만 정해두고 실제로 움직이지는 않음.
+        // 움직이는 것은 물리 업데이트에서 진행(rigidbody가 물리니까)
+        movementDirection = direction;
+    }
+
+    private void Look(Vector2 direction)
+    {
+        Rotate(direction);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        movementRigidbody.velocity = direction;
+
+        animator.SetFloat("Movement", Math.Abs(direction.x) + Math.Abs(direction.y));
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}
